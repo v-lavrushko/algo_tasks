020a15258e3109dbc650d891d27024441aecbbe4
v-lavrushko <Landadang@Gmail.com>
2021-08-21 21:51:49 +0300

    Update task_331.py

--- task_331.py
+++ task_331.py
@@ -1,6 +1,5 @@
-
-
-def quadratic_sum(n):
+@console_input
+def quadratic_sum(n=1):
     for i in range(int(n**0.5)):
         for j in range(i, int(n**0.5)):
             for k in range(j, int(n**0.5)):
@@ -8,5 +7,17 @@ def quadratic_sum(n):
                     yield i, j, k
 
 
+def console_input(func):
+    def wrapper(*args):
+        n = input("Enter a number: ")
+        task = input("Which task? ")
+        if n.isdigit() and task == "a":
+            return tuple(func(int(n)))[0]
+        elif n.isdigit() and task == "b":
+            return tuple(func(int(n)))
+        else:
+            return tuple(func())
+
+
 if __name__ == "__main__":
-    print(set(quadratic_sum(1)))
+    print(quadratic_sum())
